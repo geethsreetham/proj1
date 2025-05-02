4a67de54a8593446263e4f62048e5ef11600acd8
abyss <153100558+abys-sk@users.noreply.github.com>
2025-05-02 14:26:27 +0530

    App.js

--- login/assets/App.js
+++ login/assets/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LogoCard from './logocard';
+import LogoCard from './LogoCard';
 import LoginPage from './LoginPage';
 import ForgotPassword from './ForgotPassword';
+import Lightning from './Lightning';
+
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Initial logo page (homepage) */}
-        <Route path="/" element={<LogoCard />} />
+      <div className="relative min-h-screen w-screen">
+        {/* Lightning effect as background for all routes */}
+        <Lightning
+          hue={230}
+          xOffset={0}
+          speed={1}
+          intensity={1}
+          size={1}
+          className="absolute top-0 left-0 w-full h-full z-0"
+        />
+        
+        {/* Routes */}
+        <Routes>
+          {/* Initial logo page (homepage) */}
+          <Route
+            path="/"
+            element={
+              <div className="relative z-10">
+                <LogoCard />
+              </div>
+            }
+          />
+          
+          {/* Login page */}
+          <Route path="/login" element={<LoginPage />} />
 
-        {/*Login page */}
-        <Route path="/login" element={<LoginPage />} />
-       
-        <Route path="/forgot-Password" element={<ForgotPassword />} />
-      </Routes>
+          {/* Forgot password page */}
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
-
-
-
 
 export default App;
